b4a4d880170f3c745889bf4f02b97a97312738ac
mohdarham06 <mohdarham0634@gmail.com>
2024-02-04 19:04:12 +0530

    Created Functional Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const [active, setActive] = useState("");
+    const [isMenuChecked, setIsMenuChecked] = useState(false);
+
+    return (
+        <nav className={
+            `${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`
+        }>
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+
+                <Link
+                    to="/"
+                    className='flex items-center gap-2'
+                    onClick={() => {
+                        setActive("");
+                    }}
+                >
+                    <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+                    <p className="text-white text-[18px] font-bold cursor-pointer">Mohd Arham</p>
+                </Link>
+
+                <ul className=" list-none hidden sm:flex flex-row gap-10">
+                    {navLinks.map((link) => (
+                        <li
+                            key={link.id}
+                            className={
+                                `text-[18px] font-medium cursor-pointer hover:text-white  
+                                ${active === link.title ? "text-white" : "text-secondary"}`
+                            }
+                            onClick={() => setActive(link.title)}
+                        >
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="sm:hidden flex flex-1 justify-end items-center">
+                    <img
+                        src={isMenuChecked ? close : menu}
+                        alt="menu"
+                        className='w-[28px] h-[28px] object-contain cursor-pointer'
+                        onClick={() => setIsMenuChecked(!isMenuChecked)}
+
+                    />
+
+                    <div
+                        className={
+                            `${!isMenuChecked ? "hidden" : "flex"} 
+                            p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`
+                        }
+
+                    >
+                        <ul className="list-none flex flex-col justify-end items-start gap-4">
+                            {navLinks.map((link) => (
+                                <li
+                                    key={link.id}
+                                    className={
+                                        `text-[16px] font-poppins font-medium cursor-pointer  
+                                        ${active === link.title ? "text-white" : "text-secondary"}`
+                                    }
+                                    onClick={() => {
+                                        setActive(link.title)
+                                        setIsMenuChecked(!isMenuChecked)
+                                    }}
+                                >
+                                    <a href={`#${link.id}`}>{link.title}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
