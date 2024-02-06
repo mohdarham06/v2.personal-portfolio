4692a2b540091551f5ca71260d11c1e1c07820b3
mohdarham06 <mohdarham0634@gmail.com>
2024-02-06 16:42:50 +0530

    Created Experience and Work Section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import { motion } from 'framer-motion';
+import { BallCanvas } from './canvas';
+import { styles } from '../styles';
+import { textVariant } from '../utils/motion';
+import { technologies } from '../constants'
+import { SectionWrapper } from '../hoc';
+
+
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <div className={`${styles.sectionSubText}`}>What I U</div>
+
+                <h2 className={`${styles.sectionHeadText}`}>Technologies</h2>
+            </motion.div>
+
+            <div className='mt-20 flex flex-wrap justify-center gap-10'>
+                {technologies.map((technology) => (
+                    <div
+                        key={technology.name}
+                        className="w-20 h-28"
+                    >
+                        <BallCanvas
+                            icon={technology.icon}
+                        />
+                    </div>
+                ))}
+            </div>
+
+        </>
+
+    )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "technologies")
